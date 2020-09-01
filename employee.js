3a5a137bd75fa0d85389f9d37e9cbd08c234df3f
Edward Reyes <reyes.edwardf@gmail.com>
2020-09-01 00:58:33 -0700

    added view roles and view department queries

--- employee.js
+++ employee.js
@@ -26,33 +26,37 @@ const menu_questions = [
         ]
     }
 ];
+
 async function displayMenu() {
-    let menuChoice;
-    menuChoice = await inquirer.prompt(menu_questions)
+    let menuChoice = await inquirer.prompt(menu_questions)
         .then(function (response) {
             return response.choice;
         })
     if (menuChoice === 'View All Employees') {
-        let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager " +
-        "FROM employee as e " + 
-        "LEFT JOIN employee AS m ON e.manager_id = m.id " +
-        "INNER JOIN role ON e.role_id = role.id " + 
-        "INNER JOIN department ON role.department_id = department.id " +
-        "ORDER BY e.id";
-        getQuery(query);
+        let query = 
+            `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+            FROM employee as e
+            LEFT JOIN employee AS m ON e.manager_id = m.id
+            INNER JOIN role ON e.role_id = role.id
+            INNER JOIN department ON role.department_id = department.id
+            ORDER BY e.id`
+
+        displayTable(query);
     } else if (menuChoice === 'View All Employees By Department') {
-        let query = "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department " +
-        "FROM employee " +
-        "INNER JOIN role ON employee.role_id = role.id " +
-        "INNER JOIN department ON role.department_id = department.id " +
-        "ORDER BY employee.id"; 
-        getQuery(query);
-        console.log('View All Employees By Department');
+        let query = 
+            `SELECT employee.id, employee.first_name, employee.last_name, department.name AS department
+            FROM employee
+            INNER JOIN role ON employee.role_id = role.id
+            INNER JOIN department ON role.department_id = department.id
+            ORDER BY employee.id`; 
+
+        displayTable(query);
     } else if (menuChoice === 'View All Employees By Manager') {
-        let query = "SELECT e.id, e.last_name, CONCAT(m.first_name, ' ', m.last_name) AS manager " + 
-        "FROM employee AS e LEFT JOIN employee AS m ON e.manager_id = m.id"
-        getQuery(query);
-        console.log('View All Employees By Manager')
+        let query = 
+            `SELECT e.id, e.last_name, CONCAT(m.first_name, ' ', m.last_name) AS manager
+            FROM employee AS e LEFT JOIN employee AS m ON e.manager_id = m.id`
+
+        displayTable(query);
     } else if (menuChoice === 'Add Employee') {
         console.log('Add Employee')
     } else if (menuChoice === 'Remove Employee') {
@@ -62,13 +66,19 @@ async function displayMenu() {
     } else if (menuChoice === 'Update Employee Manager') {
         console.log('Update Employee Manager')
     } else if (menuChoice === 'View All Roles') {
-        console.log('View All Roles')
+        let query = 
+            `SELECT * FROM role`;
+        
+        displayTable(query);
     } else if (menuChoice === 'Add Role') {
         console.log('Add Role')
     } else if (menuChoice === 'Remove Role') {
         console.log('Remove Role')
     } else if (menuChoice === 'View All Departments') {
-        console.log('View All Departments')
+        let query = 
+            `SELECT * FROM department`;
+        
+        displayTable(query);
     } else if (menuChoice === 'View Total Utilized Department Budget') {
         console.log('View Total Utilized Department Budget')
     } else if (menuChoice === 'Add Department') {
@@ -82,13 +92,13 @@ async function displayMenu() {
     displayMenu();
 }
 
-const getQuery = query => {
-    console.log(query);
+
+
+const displayTable = query => {
     connection.query(query, function (err, data) {
         if (err) throw err;
-        // console.log(data);
         const table = cTable.getTable(data);
-        console.log('\n' + table);
+        console.log('\n' + table + '\n');
     })
 }
 displayMenu();
